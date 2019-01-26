82fc28039b02e24937b9b36c3c07563e8b01faf5
Sinan Buyukkardes <sinanbuyukkardes@hotmail.com>
2019-01-26 15:54:56 +0100

    initial commit

---
+++ app/src/main/java/com/company/autocompletionexample/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.company.autocompletionexample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.AppCompatAutoCompleteTextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
